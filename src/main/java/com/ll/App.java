3f485cdc40f8da8d7bac578d986b17031e489529
BukGomi <bukgomi@naver.com>
2023-11-08 08:31:32 +0900

    third

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,23 +1,34 @@
 package com.ll;
 import java.util.Scanner;
 public class App {
-    void run() {
-        System.out.println("== 명언 앱 ==");
+    public void run() {
+        System.out.println("=== 명언 ===");
+        int cnt = 0;
+
         while (true) {
             System.out.print("명령) ");
+
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
+
             if (cmd.equals("종료")) {
+                System.out.println("종료");
                 break;
-            } else if (cmd.equals("등록")) {
-                System.out.print("명언 : ");
+            }
+
+            else if (cmd.equals("등록")) {
+                System.out.print("명언: ");
                 String content = scanner.nextLine();
-                System.out.print("작가 : ");
+
+                System.out.print("작가: ");
                 String authorName = scanner.nextLine();
 
-                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
-                System.out.println("1번 명언이 등록되었습니다.");
-            }
+                cnt = cnt + 1;
+
+                System.out.println(cnt + "번 명언이 등록되었습니다.");
+                            }
+
         }
+
     }
 }
