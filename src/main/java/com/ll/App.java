4dea6560ed995918e126303c19b2de9846c0a6ec
BukGomi <bukgomi@naver.com>
2023-11-07 09:27:35 +0900

    first

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
         while (true) {
             System.out.print("명령) ");
 
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
-            System.out.printf("입력받은 명령어 : %s\n", cmd);
             if (cmd.equals("종료")) {
                 break;
             }
-            System.out.printf("입력받은 명령어 : %s\n", cmd);
         }
     }
 }
